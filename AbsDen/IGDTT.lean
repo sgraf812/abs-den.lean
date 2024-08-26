df98f99acd76c207738fbbb727ff35c430182834
Sebastian Graf <sgraf1337@gmail.com>
2024-08-26 09:23:00 +0200

    progress

--- AbsDen/IGDTT.lean
+++ AbsDen/IGDTT.lean
@@ -6,32 +6,48 @@ namespace IGDTT
 open Lean Lean.Parser
 
 universe u
+
+def Laterrr (α : Type u) : Type u := Unit → α
+def nexttt {α : Type u} (a : Unit → α) : Laterrr α := a
+def appp {α β : Type u} (f : Laterrr (α → β)) (a : Laterrr α) : Laterrr β :=
+  fun _ => f () (a ())
+unsafe def gfixxx {α : Type u} (f : Laterrr α → α) : α := f (nexttt (fun () => gfixxx f))
+
+-- Generalise from Type u to Sort u:
+@[extern "Laterrr"]
 axiom Later : Sort u → Sort u
-axiom next {α : Sort u} : α → Later α
-axiom ap {α β : Sort u} : Later (α → β) → Later α → Later β
+@[extern "nexttt"]
+axiom next {α : Sort u} (a : PUnit → α) : Later α
+@[extern "appp"]
+axiom ap {α β : Sort u} (f : Later (α → β)) (a : Later α) : Later β
+@[extern "gfixxx"]
+axiom gfix {α : Sort u} (f : Later α → α) : α
+axiom gfix.unfold {α : Sort u} (f : Later α -> α) : gfix f = f (next (fun () => gfix f))
+
 notation "▹ " α:100 => Later α
 
-noncomputable instance : Applicative Later where
-  pure := next
+instance : Applicative Later where
+  pure a := next (fun () => a)
   seq f a := ap f (a ())
-
-axiom Later.eq {α : Sort u} (a b : α) : ▹ (a = b) ↔ next a = next b
-axiom ap.compute {α β : Sort u} (f : α → β) (a : α) : ap (next f) (next a) = next (f a)
-axiom ap.id {α : Sort u} (a : ▹ α) : ap (next id) a = a
-
-axiom gfix {α : Sort u} : (▹ α → α) → ▹ α
-axiom gfix.unfold {α : Sort u} (f : ▹ α -> α) : gfix f = next (f (gfix f))
-
-axiom DLater : ▹ (Sort u) → Sort u
-axiom DLater.next_eq {α : Sort u} : ▹ α = DLater (next α)
-notation "▸ " α:100 => DLater α
 
 declare_syntax_cat nextElem
 syntax (name := nextElem) ident Term.optType ppSpace Term.leftArrow term : nextElem
 syntax (name := delayedSubst) "next[" nextElem,* "]." term : term
 macro_rules
-  | `(term| next[]. $e) => `(pure $e)
+  | `(term| next[]. $e) => `(next (fun () => $e))
   | `(term| next[ $x:ident ← $e₁ ]. $e₂) => `(pure (fun $x => $e₂) <*> $e₁)
   | `(term| next[ $x:ident : $t ← $e₁ ]. $e₂) => `(pure (fun $x : $t => $e₂) <*> $e₁)
   | `(term| next[ $x:ident ← $e₁, $es,* ]. $e₂) => `((next[ $es,* ]. (fun $x => $e₂)) <*> $e₁)
   | `(term| next[ $x:ident : $t ← $e₁, $es,* ]. $e₂) => `((next[ $es,* ]. (fun $x : $t => $e₂)) <*> $e₁)
+
+axiom Later.eq {α : Sort u} {a b : α} : ▹ (a = b) ↔ (next[].a) = (next[].b)
+axiom ap.compute {α β : Sort u} (f : α → β) (a : α) : ap (next[].f) (next[].a) = next[].(f a)
+axiom ap.id {α : Sort u} (a : ▹ α) : ap (next[].id) a = a
+
+axiom DLater : ▹ (Sort u) → Sort u
+axiom DLater.next_eq {α : Sort u} : ▹ α = DLater (next[].α)
+notation "▸ " α:100 => DLater α
+
+-- protected def Later.recOn {α : Type u} {motive : ▹ α → Type u} : (l : ▹ α) → ▹ ((a : α) → motive (next a)) → motive l :=
+--   cannot have this! Otherwise, it would be possible to recOn (▹ False)
+--   sorry
--- AbsDen/Semantics.lean
+++ AbsDen/Semantics.lean
@@ -9,13 +9,13 @@ inductive Event where
   | upd | app1 | app2 | let1 | case1 | case2
 
 class Trace (d : Type) where
-  step : Event → d → d
+  step : Event → ▹ d → d
 open Trace
 
-class Domain (d : Type) where
+class Domain (a : Type) (d : Type) where
   stuck : d
-  fn : Name → (d → d) → d
-  ap : d → d → d
+  fn : Name → (a → d) → d
+  ap : d → a → d
 --  con : ConTag → List d → d
 --  select : d → AList ConTag (List d → d) → d
 open Domain
@@ -24,32 +24,116 @@ class HasBind (d : Type) where
   bind : Name → (d → d) → (d → d) → d
 open HasBind
 
-def eval [Trace d] [Domain d] [HasBind d] (ρ : FinMap Name d) : Exp → d
+def eval [Trace d] [Domain d d] [HasBind d] (ρ : FinMap Name d) : Exp → d
   | Exp.var x => match AList.lookup x ρ with
-      | Option.none   => stuck
+      | Option.none   => @stuck d _ _
       | Option.some d => d
-  | Exp.lam x e => fn x (λ d => step Event.app2 (eval (ρ[x↦d]) e))
+  | Exp.lam x e => fn x (λ d => step Event.app2 (next[]. eval (ρ[x↦d]) e))
   | Exp.app e x => match AList.lookup x ρ with
-      | Option.none   => stuck
-      | Option.some d => step Event.app1 (ap (eval ρ e) d)
-  | Exp.let x e₁ e₂ => bind x (λ d₁ =>                  eval (ρ[x↦step (Event.look x) d₁]) e₁)
-                              (λ d₁ => step Event.let1 (eval (ρ[x↦step (Event.look x) d₁]) e₂))
+      | Option.none   => @stuck d _ _
+      | Option.some d => step Event.app1 (next[]. ap (eval ρ e) d)
+  | Exp.let x e₁ e₂ => bind x (λ d₁ =>                          eval (ρ[x↦step (Event.look x) (next[]. d₁)]) e₁)
+                              (λ d₁ => step Event.let1 (next[]. eval (ρ[x↦step (Event.look x) (next[]. d₁)]) e₂))
 
-inductive T.F (α : Type) (τ : ▹ Type) : Type where
+inductive T.F (α : Type u) (τ : ▹ (Type u)) : Type u where
   | ret : α → T.F α τ
   | step : Event → ▸ τ → T.F α τ
-def T α := T.F α (gfix (T.F α))
+def T α := gfix (T.F α)
+theorem T.unfold : T α = T.F α (next[]. T α) := gfix.unfold (T.F α)
+instance : Coe (T α) (T.F α (next[]. T α)) where
+  coe := cast T.unfold
+instance : Coe (T.F α (next[]. T α)) (T α) where
+  coe := cast T.unfold.symm
+def T.ret (x : α) : T α := cast T.unfold.symm (T.F.ret x)
+def T.step (e : Event) (tl : ▹ T α) : T α := cast T.unfold.symm (T.F.step e (cast DLater.next_eq tl))
+protected def T.recOn.{v,u} {α : Type u} {motive : T α  → Sort v} (t : T α)
+  (ret : (a : α) → motive (T.ret a))
+  (step : (e : Event) → (tl : ▹ T α) → motive (T.step e tl))
+  : motive t := by
+    let motive2 : T.F α (next[]. T α) → Sort v := motive ∘ cast T.unfold.symm
+    let fw {t} : motive (cast T.unfold.symm t) → motive2 t := cast (by aesop)
+    let bw {t} : motive2 (cast T.unfold t) → motive t := cast (by aesop)
+    have h : motive2 (cast T.unfold t) :=
+      let go : (t : T.F α (next[]. T α)) → motive2 t
+             | .ret a     => fw (ret a)
+             | .step e tl => cast (by aesop) (fw (step e (cast DLater.next_eq.symm tl)))
+      go (cast T.unfold t)
+    exact bw h
+def T.bind {α β} (t : T α) (k : α → T β) : T β :=
+  let loop loop' t : T β := match cast T.unfold t with
+    | .ret a     => k a
+    | .step e tl => T.step e (next[loop ← loop', t ← cast DLater.next_eq.symm tl]. loop t)
+  gfix loop t
 
-inductive Value.Tag : Type where
-  | stuck
-  | fun
-def Value.F (d : ▹ Type) : Type := @Sigma Value.Tag fun
-  | .stuck => unit
-  | .fun   => (Name × ▸ d → T (Value.F d))
-inductive ValueF (d : ▹ Type) : Type where
-  | stuck : ValueF d
-  | fun : (Name × ▸ d → T (ValueF d)) → ValueF d
-  --  | con : Con → List (Name × ▸ d) → ValueF d
+instance : Trace (T α) where
+  step := T.step
 
-def Value := ValueF (gfix (T ∘ ValueF))
-def D := T Value
+instance : Monad T where
+  pure := T.ret
+  bind := T.bind
+
+def Heap (n : ▹ Type) := Nat → ▸ n
+structure ByNeed.F (d : ▹ Type) (α : Type) : Type where
+  mk :: f : Heap d → T (Heap d × α)
+
+inductive Value.F (p : Type) (n : ▹ Type) : Type where
+  | stuck : Value.F p n
+  | fun : (▸ n → p) → Value.F p n
+-- trustMeFix is safe for any monotone `f`
+partial def trustMeFix [Nonempty α] (f : α → α) := f (trustMeFix f)
+axiom trustMeFix.unfold {α} [Nonempty α] (f : α → α) : trustMeFix f = f (trustMeFix f)
+-- axiom trustMeFix.ind {α} [Nonempty α] {f : α → α} {g : α → α} : ((x:α) → f x = g x) → trustMeFix f = trustMeFix g
+-- axiom trustMeFix.fixpoint {α} [Nonempty α] {f : α → α} {x : α} : (f x = x) → trustMeFix f = x
+-- the functional below is clearly monotone because p only occurs in positive position,
+-- so D.F is a proper inductive datatype.
+def D.F (p : Type) (n : ▹ Type) := ByNeed.F n (Value.F p n)
+def D := gfix (fun n => trustMeFix (fun p => D.F p n))
+theorem D.unfold : D = D.F D (next[].D) := by
+  calc D = gfix (fun n => trustMeFix (fun p => D.F p n)) := rfl
+       _ = trustMeFix (fun p => D.F p (next[]. D)) := gfix.unfold _
+       _ = D.F (trustMeFix (fun p => D.F p (next[]. D))) (next[]. D) := trustMeFix.unfold _
+       _ = D.F D (next[].D) := congrArg (D.F · (next[]. D)) (gfix.unfold (fun n => trustMeFix (fun p => D.F p n))).symm
+instance : Coe D (D.F D (next[]. D)) where
+  coe := cast D.unfold
+instance : Coe (D.F D (next[]. D)) D where
+  coe := cast D.unfold.symm
+
+abbrev Value := Value.F D (next[].D)
+def Value.stuck : Value := Value.F.stuck
+def Value.fun (f : ▹ D → D) : Value := Value.F.fun (f ∘ cast DLater.next_eq.symm)
+
+abbrev ByNeed α := ByNeed.F (next[]. D) α
+
+theorem D.eq : D = ByNeed Value := by
+  calc D = D.F D (next[].D) := D.unfold
+       _ = ByNeed Value := rfl
+instance : Coe D (ByNeed Value) where
+  coe := cast D.eq
+instance : Coe (ByNeed Value) D where
+  coe := cast D.eq.symm
+
+def ByNeed.mk (f : Heap (next[]. D) → T (Heap (next[]. D) × α)) : ByNeed α :=
+  ByNeed.F.mk f
+def ByNeed.f (d : ByNeed α) : Heap (next[]. D) → T (Heap (next[]. D) × α) :=
+  match d with | ByNeed.F.mk f => f
+
+abbrev D.f (d : D) := @ByNeed.f Value d
+abbrev D.bv : ByNeed Value → D := cast D.eq.symm
+abbrev D.unBv : ByNeed Value → D := cast D.eq.symm
+
+instance : Trace D where
+  step e tl := ByNeed.mk fun μ => step e (next[d ← tl]. d.f μ)
+
+instance : Monad ByNeed where
+  pure a := ByNeed.mk fun μ => T.ret ⟨μ, a⟩
+  bind a k := ByNeed.mk fun μ => a.f μ >>= fun
+    | ⟨μ₂, r⟩ => ByNeed.f (k r) μ₂
+
+instance : Domain (▹ D) D where
+  stuck := D.mk <| pure Value.stuck
+  fn f := D.mk <| pure (Value.fun f)
+  ap d a := D.mk do
+    let v ← d
+    match v with
+    | .fun f => f a
+    | _      => stuck
